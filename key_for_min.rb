f04ca84ba5328867d20298f9644e85f3fe1d4a5d
Karl Jerrenny Ojeda <karlojeda@yahoo.com>
2019-11-16 07:41:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,13 +5,13 @@ def key_for_min_value(name_hash)
   if name_hash == {}
     return nil
   else
-    puts min_key = name_hash.first[0]
-    # name_hash.collect do |key, value|
+    min_key = name_hash.first[0]
+    name_hash.collect do |key, value|
   
-    #   if key[min_key] <= value
-    #     puts "#{min_key} is now #{value}."
-    #   end
-    #     min_key = key
-    # end
+      if key[min_key] <= value
+        puts "#{min_key} is now #{value}."
+      end
+        min_key = key
+    end
   end
 end
