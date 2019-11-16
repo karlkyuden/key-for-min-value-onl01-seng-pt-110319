a9b9aed3ae734c5ee6fc7ad1aa9aebbb2c67d8a7
Karl Jerrenny Ojeda <karlojeda@yahoo.com>
2019-11-16 07:40:48 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   if name_hash = {}
     return nil
   else
-    puts min_key = name_hash.first[1]
+    puts min_key = name_hash.first[0]
     # name_hash.collect do |key, value|
   
     #   if key[min_key] <= value
